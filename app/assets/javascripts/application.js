ae69542cb657acf293349b44e7a38aaf7a422a33
Dejan22 <dejan.tomasevic@protonmail.com>
2017-12-30 21:49:48 +0100

    some initial AJAX

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,3 +4,29 @@
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require_tree .
+function createSong(title) {
+
+  var newSong = { title: title};
+
+  $.ajax({
+    type: "POST",
+    url: "/artists/:artist_id/songs/:id/songs.json",
+    data: JSON.stringify({
+        song: newSong
+    }),
+    contentType: "application/json",
+    dataType: "json"
+  });
+}
+
+function deleteSong(songId) {
+  $.ajax({
+    type: "DELETE",
+    url: "/artists/:artist_id/song.json",
+    contentType: "application/json",
+    dataType: "json"
+  })/artists/artist_id/song.json
+  .done(function(data) {
+    $('tr[data-id="'+title+'"]').remove();
+  });
+}
