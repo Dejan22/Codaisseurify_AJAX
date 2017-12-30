3468408a067ad977ae01f5c0f2226ccf0215206e
Dejan22 <dejan.tomasevic@protonmail.com>
2017-12-30 17:06:48 +0100

    migrated and fixed bugs, app works

--- db/schema.rb
+++ db/schema.rb
@@ -10,48 +10,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170121212434) do
+ActiveRecord::Schema.define(version: 20171230145909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "artists", force: :cascade do |t|
-    t.string   "artist_name"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.date     "birthdate"
-    t.string   "genre"
-    t.string   "image_url"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+  create_table "artists", id: :serial, force: :cascade do |t|
+    t.string "artist_name"
+    t.string "first_name"
+    t.string "last_name"
+    t.date "birthdate"
+    t.string "genre"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "songs", force: :cascade do |t|
-    t.string   "title"
-    t.string   "album"
-    t.integer  "album_track_number"
-    t.date     "release_date"
-    t.integer  "artist_id"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.index ["artist_id"], name: "index_songs_on_artist_id", using: :btree
+  create_table "songs", id: :serial, force: :cascade do |t|
+    t.string "title"
+    t.string "album"
+    t.integer "album_track_number"
+    t.date "release_date"
+    t.integer "artist_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_songs_on_artist_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "songs", "artists"
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,9 @@
-require 'rails_helper'
+require 'test_helper'
 
-RSpec.describe PagesController, type: :controller do
-
-  describe "GET #home" do
-    it "returns http success" do
-      get :home
-      expect(response).to have_http_status(:success)
-    end
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get pages_home_url
+    assert_response :success
   end
 
 end
